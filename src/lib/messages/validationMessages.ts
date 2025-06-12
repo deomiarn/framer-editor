33ea0d6e11d150a6df5a1dcf59759f2a7b25489f
deomiarn <deomicarnel@hotmail.com>
2025-06-12 17:16:03 +0200

    feat: update password requirements

--- src/lib/messages/validationMessages.ts
+++ src/lib/messages/validationMessages.ts
@@ -1,4 +1,8 @@
 export const validationMessages = {
   email: "Valid E-Mail required",
-  passwordMin: "Password must be at least 6 characters long"
+  passwordMin: "Password must be at least 8 characters long",
+  passwordLowercase: "Password must include at least one lowercase letter",
+  passwordUppercase: "Password must include at least one uppercase letter",
+  passwordNumber: "Password must include at least one number",
+  passwordSymbol: "Password must include at least one special character"
 };
--- src/lib/schemas/authSchema.ts
+++ src/lib/schemas/authSchema.ts
@@ -3,7 +3,14 @@ import { validationMessages } from "@/lib/messages/validationMessages.ts";
 
 export const authSchema = z.object({
   email: z.string().email({ message: validationMessages.email }),
-  password: z.string().min(6, { message: validationMessages.passwordMin })
+
+  password: z
+    .string()
+    .min(8, { message: validationMessages.passwordMin })
+    .regex(/[a-z]/, { message: validationMessages.passwordLowercase })
+    .regex(/[A-Z]/, { message: validationMessages.passwordUppercase })
+    .regex(/[0-9]/, { message: validationMessages.passwordNumber })
+    .regex(/[^a-zA-Z0-9]/, { message: validationMessages.passwordSymbol })
 });
 
 export type AuthSchema = z.infer<typeof authSchema>;
